b814b0d5b7abeaa200aa84b23b276749142b3774
Filip Sufitchi <fsufitchi@gmail.com>
2015-04-15 16:54:27 -0400

    display all mails on front page

--- src/omnomnom/webui/handlers/frontpage.py
+++ src/omnomnom/webui/handlers/frontpage.py
@@ -1,5 +1,17 @@
 from tornado.web import RequestHandler
 
+from omnomnom.common.db import manager as db_manager
+from omnomnom.common.db import Email
+
 class FrontPageHandler(RequestHandler):
     def get(self):
-        self.write("hi")
+        self.set_header("Content-type", "text/plain")
+        self.write("hi\n")
+
+        session = db_manager.create_session()
+        for email in session.query(Email).all():
+            self.write("FROM: " + email.from_addr)
+            self.write("TO: " + email.to_addrs)
+            self.write("SUBJECT: " + email.subject)
+            self.write("BODY:\n" + email.body)
+            self.write("\n\n============ END BODY ==========\n\n")
